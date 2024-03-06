602ee83797f5976304fd2c4354ba2401c2b0bf3a
Paul Thompson <pmt10@case.edu>
2024-03-06 10:45:02 -0500

    add testing file to generate voltage traces

--- test/generate_voltage_traces.py
+++ test/generate_voltage_traces.py
@@ -3,19 +3,18 @@ from spikesorting_fullpursuit.test import gen_dataset
 
 
 def generate_voltage_traces():
-
     n_chans = 4  # Number of channels to make in test dataset
     v_duration = 30  # Test dataset duration in seconds
     random_seed = 99  # Set seed of numpy random number generator for spike times
     neuron_templates = None  # Just use default pre-loaded template waveforms to generate spike voltage traces
     frequency_range = (300, 6000)  # Frequencies of dataset in Hz
     samples_per_second = 40000  # Sampling rate of 40kHz
-    amplitude = 1  # Amplitude of 3 standard deviations of noise
+    amplitude = 1  # Amplitude of 1 standard deviations of noise
     percent_shared_noise = 0.3  # Create shared noise across channels
     correlate1_2 = (
         0.10,
         10,
-    )  # Set 15% of neuron 2 spikes to occur within 10 samples of a neuron 1 spike
+    )  # Set 10% of neuron 2 spikes to occur within 10 samples of a neuron 1 spike
     electrode_type = "tetrode"  # Choose pre-loaded electrode type of tetrode and all channels in neighborhood
     voltage_dtype = np.float32  # Create voltage array as float 32
 
@@ -50,3 +49,16 @@ def generate_voltage_traces():
     )
 
     return test_data.Probe.voltage, test_data.actual_IDs
+
+
+def generate_2d_voltage_array():
+
+    voltages, timestamps = generate_voltage_traces()
+
+    voltages = np.float32(voltages)
+
+    raw_voltages = np.frombuffer(voltages, dtype=np.float32).reshape(
+        (4, len(voltages[0]))
+    )
+
+    return raw_voltages, timestamps
